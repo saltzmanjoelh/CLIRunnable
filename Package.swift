3588acb66f157d2464cb992ff6c7d83679451010
Joel Saltzman <talk@joelsaltzman.com>
2017-09-10 11:13:29 -0700

    Updated to swift 4 package

--- Package.swift
+++ Package.swift
@@ -1,7 +1,28 @@
 // swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "CliRunnable",
-    dependencies: [.Package(url: "https://github.com/saltzmanjoelh/YamlSwift.git", versions: Version(0,0,0)..<Version(10,0,0))]
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "CliRunnable",
+            targets: ["CliRunnable"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/saltzmanjoelh/Yaml", from: "3.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "CliRunnable",
+            dependencies: ["Yaml"]),
+        .testTarget(
+            name: "CliRunnableTests",
+            dependencies: ["CliRunnable", "Yaml"]),
+    ]
 )
