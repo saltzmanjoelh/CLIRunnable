c1b34f3864dbe9073c94779b3cf5890080fdbd70
Joel Saltzman <talk@joelsaltzman.com>
2018-03-26 00:12:43 -0700

    Fixed bad static type

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "CliRunnable",
-            type: static,
+            type: .static,
             targets: ["CliRunnable"]),
     ],
     dependencies: [
